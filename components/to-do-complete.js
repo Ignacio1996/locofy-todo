a23c8921be4f84cc12ecea2d5529a81a01633ef5
Ignacio1996 <ec2-user@ip-10-11-203-135.ap-southeast-1.compute.internal>
2023-04-28 18:27:48 +0000

    Pushed from Locofy Builder

--- components/to-do-complete.js
+++ components/to-do-complete.js
@@ -4,7 +4,6 @@ const ToDoComplete = ({ taskValue }) => {
     <div className={styles.todocomplete}>
       <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
       <div className={styles.finishDesigns}>{taskValue}</div>
-      <img className={styles.vectorIcon} alt="" src="/vector.svg" />
     </div>
   );
 };
--- components/to-do-incomplete.js
+++ components/to-do-incomplete.js
@@ -4,7 +4,6 @@ const ToDoIncomplete = ({ taskValue }) => {
     <div className={styles.todoincomplete}>
       <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
       <div className={styles.makeResponsiveOn}>{taskValue}</div>
-      <img className={styles.vectorIcon} alt="" src="/vector.svg" />
     </div>
   );
 };
